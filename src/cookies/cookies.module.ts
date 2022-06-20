3ed7950da7438a25c9f891e6a75b89121fe4c4ee
Frederik Ingwersen <frederik.ingwersen@gmail.com>
2022-06-20 18:24:10 +0200

    Remove CookiesController from module

--- src/cookies/cookies.module.ts
+++ src/cookies/cookies.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { CookiesService } from './cookies.service';
-import { CookiesController } from './cookies.controller';
-import { CookiesResolver } from './cookies.resolver';
+import { Module } from "@nestjs/common";
+
+import { CookiesResolver } from "./cookies.resolver";
+import { CookiesService } from "./cookies.service";
 
 @Module({
   providers: [CookiesService, CookiesResolver],
-  controllers: [CookiesController]
+  exports: [CookiesService],
 })
 export class CookiesModule {}
